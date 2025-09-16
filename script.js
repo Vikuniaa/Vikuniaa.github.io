ffe43023083aa58637ce771fad7acc7e9e9ace10
Vikuniaa <146185805+Vikuniaa@users.noreply.github.com>
2025-09-16 16:56:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const kotek_kursor = document.querySelector('.custom-cursor .kotek-kursor');
+
+
+const kotek_kursor = document.querySelector('.custom-cursor.kotek-kursor');
+
+document.addEventListener('mouseenter' , () => {
+
+ kotek_kursor.style.display = 'block';
+
+});
+
+document.addEventListener('mouseleave' , () => {
+
+ kotek_kursor.style.display = 'none';
+
+});
+
+document.addEventListener('mousemove', TrackCursor);
+
+function TrackCursor(evt){
+
+    const w = kotek_kursor.clientWidth;
+    const h = kotek_kursor.clientHeight;
+     
+    kotek_kursor.style.transform = `translate(${evt.clientX - w/2}px, ${evt.clientY - h/2}px)`;
+}
